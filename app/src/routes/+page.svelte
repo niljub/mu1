3e81fbea8125f8a87e5f196b8869afeff03f7bf2
Nico Ljubicic <nico.ljubicic@stud.h-da.de>
2023-10-27 13:38:36 +0200

    added two registers for testing -> need to refactor the mu1 emulator

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -29,17 +29,17 @@
 			case 0x01: // Load value into accumulator
 				mu1.acc = mu1.memory[mu1.pc + 1];
 				mu1.pc += 2;
-				drawRect(mu1.acc);
+				drawStuff(mu1.acc, mu1.pc);
 				break;
 			case 0x02: // Store accumulator value to memory
 				mu1.memory[mu1.pc + 1] = mu1.acc;
 				mu1.pc += 2;
-				drawRect(mu1.acc);
+				drawStuff(mu1.acc, mu1.pc);
 				break;
 			case 0x03: // Add value to accumulator
 				mu1.acc += mu1.memory[mu1.pc + 1];
 				mu1.pc += 2;
-				drawRect(mu1.acc);
+				drawStuff(mu1.acc, mu1.pc);
 				break;
 			// Add more cases for other instructions
 			default:
@@ -51,68 +51,105 @@
 	onMount(() => {
 		const program = [0x01, 0x05, 0x03, 0x08, 0x02, 0x00]; // Sample program bytes
 		loadProgram(program);
+		drawStuff();
 	});
 	function roundRect(ctx, x, y, width, height, radius, fill, stroke) {
-  if (typeof stroke == "undefined" ) {
-    stroke = true;
-  }
-  if (typeof radius === "undefined") {
-    radius = 5;
-  }
-  ctx.beginPath();
-  ctx.moveTo(x + radius, y);
-  ctx.lineTo(x + width - radius, y);
-  ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-  ctx.lineTo(x + width, y + height - radius);
-  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-  ctx.lineTo(x + radius, y + height);
-  ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-  ctx.lineTo(x, y + radius);
-  ctx.quadraticCurveTo(x, y, x + radius, y);
-  ctx.closePath();
-  if (stroke) {
-    ctx.stroke();
-  }
-  if (fill) {
-    ctx.fill();
-  }        
-}
+		if (typeof stroke == "undefined") {
+			stroke = true;
+		}
+		if (typeof radius === "undefined") {
+			radius = 5;
+		}
+		ctx.beginPath();
+		ctx.moveTo(x + radius, y);
+		ctx.lineTo(x + width - radius, y);
+		ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+		ctx.lineTo(x + width, y + height - radius);
+		ctx.quadraticCurveTo(
+			x + width,
+			y + height,
+			x + width - radius,
+			y + height
+		);
+		ctx.lineTo(x + radius, y + height);
+		ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+		ctx.lineTo(x, y + radius);
+		ctx.quadraticCurveTo(x, y, x + radius, y);
+		ctx.moveTo(25, -100);
 
-	function drawRect(value) {
+		ctx.closePath();
+		if (stroke) {
+			ctx.stroke();
+		}
+		if (fill) {
+			ctx.fill();
+		}
+	}
+
+	function drawAcc(value) {
 		if (document.getElementById("myCanvas") != null) {
 			const canvas = document.getElementById("myCanvas");
 			const ctx = canvas.getContext("2d");
+			
+			var rectX = 25;
+			var rectY = 450;
 
 			ctx.lineWidth = 4;
 			ctx.strokeStyle = "#000000";
 			ctx.fillStyle = "#abc";
-			roundRect(ctx, 10, 10, 100, 50, 10, true);
+			roundRect(ctx, rectX, rectY, 100, 50, 10, true);
 			ctx.font = "20px Georgia";
 			ctx.textAlign = "center";
 			ctx.textBaseline = "middle";
 			ctx.fillStyle = "#000000";
 			var rectHeight = 50;
 			var rectWidth = 100;
-			var rectX = 10;
-			var rectY = 10;
-			ctx.fillText(
-				value,
-				rectX + rectWidth / 2,
-				rectY + rectHeight / 2
-			);
+			ctx.fillText(value, rectX + rectWidth / 2, rectY + rectHeight / 2);
+			ctx.fillText("ACC", rectX + rectWidth / 2, rectY-15);
 		} else {
 			alert("Canvas not found!");
 		}
+	}
+
+
+	function drawPc(value) {
+		if (document.getElementById("myCanvas") != null) {
+			const canvas = document.getElementById("myCanvas");
+			const ctx = canvas.getContext("2d");
+			
+			var rectX = 150;
+			var rectY = 450;
+			
+			ctx.lineWidth = 4;
+			ctx.strokeStyle = "#000000";
+			ctx.fillStyle = "#abc";
+			roundRect(ctx, rectX, rectY, 100, 50, 10, true);
+			ctx.font = "20px Georgia";
+			ctx.textAlign = "center";
+			ctx.textBaseline = "middle";
+			ctx.fillStyle = "#000000";
+			var rectHeight = 50;
+			var rectWidth = 100;
+			ctx.fillText(value, rectX + rectWidth / 2, rectY + rectHeight / 2);
+			ctx.fillText("PC", rectX + rectWidth / 2, rectY-15);
+		} else {
+			alert("Canvas not found!");
+		}
+	}
+
+
+	function drawStuff(acc = "NULL", pc = "NULL"){
+		drawAcc(acc);
+		drawPc(pc);
 	}
 </script>
 
 <div>
 	<canvas
 		id="myCanvas"
-		width="300"
-		height="150"
+		width="600"
+		height="600"
 		style="border:1px solid grey"
-		on:click={drawRect("test")}
 	/>
 
 	<h2>Current Values</h2>
